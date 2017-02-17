e9a1a5aa3fae97611eae1f490252d1554efff22f
Adrian Somesan <adrian.gabriel.somesan@sap.com>
2017-02-17 21:53:37 +0200

    First commit

---
+++ jenkinsTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jenkinsTest
+//
+//  Created by Somesan, Adrian Gabriel on 17/02/2017.
+//  Copyright © 2017 Somesan, Adrian Gabriel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
